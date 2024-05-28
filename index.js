a980a29f075f3ac0d2a945b60ae0a4664d52342c
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-05-28 21:36:41 +0300

    + add express example

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-import http from 'http';
+import express from 'express';
 
-const host = '127.0.0.1';
 const port = 8000;
 
-const server = http.createServer((req, res) => {
-    switch (req.method) {
-        case 'GET':
-            switch (req.url) {
-                case '/hello':
-                    res.statusCode = 200;
-                    res.setHeader('Content-Type', 'text/plain');
-                    res.end('Hi!');
-                    break;
-            }
-            break;
-    }
+const app = express();
+
+app.get('/hello', (req, res) => {
+    res.end('Hi!');
 });
 
-server.listen(port, host, () => {
-    console.log(`Server running on ${host}:${port}`);
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
 });

7ca6aa0b6afc4f13df66599e25d3351a088301fb
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-05-28 21:59:05 +0300

    + add user router

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from 'express';
+import { userRouter } from './users/users.js';
 
 const port = 8000;
 
 const app = express();
 
 app.get('/hello', (req, res) => {
-    res.end('Hi!');
+    // res.end('Hi!');
+    res.end();
 });
+
+app.use('/users', userRouter);
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);

d6236947ce98290d2c980fee91b34e8104f80aea
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-06-03 03:26:23 +0300

    + add exception filter

---
+++ src/errors/exception.filter.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from 'express';
+import { LoggerService } from '../logger/logger.service';
+import { IExceptionFilter } from './exception.filter.interface';
+import { HttpError } from './http-error.class';
+
+export class ExceptionFilter implements IExceptionFilter {
+    logger: LoggerService
+    constructor(logger: LoggerService) {
+        this.logger = logger;
+    }
+    catch(err: Error | HttpError, req: Request, res: Response, next: NextFunction) {
+        if (err instanceof HttpError) {
+            this.logger.error(`[${err.context}] Error ${err.statusCode}: ${err.message}`);
+        } else {
+            this.logger.error(`${err.message}`);
+            res.status(500).send({err: err.message});
+        }
+    }
+}

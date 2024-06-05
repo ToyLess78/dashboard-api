e08475bf8229c7e21b34a14f3396cf8e59451081
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-06-05 22:57:44 +0300

    + update exception.filter

--- src/errors/exception.filter.ts
+++ src/errors/exception.filter.ts
@@ -1,13 +1,15 @@
 import { NextFunction, Request, Response } from 'express';
-import { LoggerService } from '../logger/logger.service';
+import { inject, injectable } from 'inversify';
+import { ILogger } from '../logger/logger.interface';
+import { TYPES } from '../types';
 import { IExceptionFilter } from './exception.filter.interface';
 import { HttpError } from './http-error.class';
+import 'reflect-metadata';
 
+@injectable()
 export class ExceptionFilter implements IExceptionFilter {
-    logger: LoggerService
-    constructor(logger: LoggerService) {
-        this.logger = logger;
-    }
+    constructor(@inject(TYPES.ILogger) private logger: ILogger) { }
+
     catch(err: Error | HttpError, req: Request, res: Response, next: NextFunction) {
         if (err instanceof HttpError) {
             this.logger.error(`[${err.context}] Error ${err.statusCode}: ${err.message}`);
